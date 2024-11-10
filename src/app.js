0e16cd44bf71a7feb178064c0d3cc318e37cb677
Venkata Charan Chinni <chinnicharan14@gmail.com>
2024-11-10 18:40:46 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,44 +2,34 @@ const express = require("express");
 
 const app = express();
 
-// This will only handle GET call to /user
-// "/ab+c" ------> here b can come anynumber of times (Eg : abbbc,abc,abbbbbbc,abbbbbbbbbbbc)
-// "/ab?c" ------> here b is optional (Eg : abc, ac)
-// "/ab*c" ------> here we can write anything between b and c it will work (Eg : abNikhilc, absdfsdc, abramc,abc)
-// "/a(bc)+d" ----> here we can get bc any number of times (Eg : abcd, abcbcd)
-// Note : Similary we can do the above grouping for * and ? also .
-// so this regex /a/ denotes that will work for if any of the word contains a it will work (Eg : a, ram, krishna, aaa, abc)
-// /.*fly$/ So this regex denotes that anything before fly can be there and at the end fly should be there (Eg : fly, butterfly, dragonfly)
-// http://localhost:3000/test?name=nikhil&age=28  We can read this query using req.query (Output will be request query { name: 'nikhil', age: '28' })
-// app.get("/test",(req,res)=>{
-//     console.log("request params", req.query);
-//     res.send({firstName:"Chinni",lastName:"Nikhil"});
-// })
-// http://localhost:3000/test/897  We can read the dynamic routes using req.params (Output will be dynamic req params { id: '897' })
-// app.get("/test/:id",(req,res)=>{
-//     console.log("request params", req.params);
-//     res.send({firstName:"Chinni",lastName:"Nikhil"});
-// })
-
-
-
-
-// This app.use will macth all HTTP API method calls 
-// app.use("/hello/2", (req, res) => {
-//   res.send("Hello from server 2");
-// });
-
-// app.use("/hello", (req, res) => {
-//   res.send("Hello from server");
-// });
-
-// app.use("/test", (req, res) => {
-//   res.send("This is a test link");
-// });
-
-// app.use("/", (req, res) => {
-//   res.send("Root Path");
-// });
+// rH --> denotes route handlers and we can have as many as route handlers 
+//app.use("/route", rH, [rH2, rH3], rH4, rh5);
+app.get(
+    "/user",
+    (req, res, next) => {
+      console.log("Handling the route user!!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling the route user 2!!");
+      // res.send("2nd Response!!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling the route user 3!!");
+      // res.send("3rd Response!!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling the route user 4!!");
+      // res.send("4th Response!!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling the route user 5!!");
+      res.send("5th Response!!");
+    }
+  );
 
 app.listen(3000, () => {
   console.log("Server is successfully listening at port 3000");
