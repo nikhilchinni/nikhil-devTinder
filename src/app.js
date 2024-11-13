0c74bf61499f9941e554323ffaaa701e6986f166
Chinni Nikhil <chinninikhil06@gmail.com>
2024-11-13 19:46:24 +0530

    getAPIs

--- src/app.js
+++ src/app.js
@@ -16,6 +16,40 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// API- Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    console.log(userEmail);
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+    // const user = await User.find({ emailId: userEmail });
+    // if (user.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(user);
+    // }
+  } catch {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// API - Feed API - GET /feed - get all the users from the database
+
+app.get("/feed",async (req,res)=>{
+    try{
+      const users = await User.find({});
+      res.send(users);
+    }
+    catch{
+      res.status(400).send("Something went wrong");
+    }
+})
+
 connectDB()
   .then(() => {
     console.log("Database Connection established............");
